7dbaaf4ea2a0e5f9b9ff56cac6d0c1064bb8b64d
Dopple24 <roubradek1@gmail.com>
2025-12-26 21:23:39 +0100

    dev: implements qr code from inputs

--- query_reader.js
+++ query_reader.js
@@ -1,6 +1,22 @@
+import init, { create_qr } from './qr_creator/pkg/qr_creator.js';
+
 const urlParams = new URLSearchParams(window.location.search);
+const iban = urlParams.get('iban');
 
-// Get a specific parameter
-const id = urlParams.get('id');
+async function run() {
+    const amount = document.getElementById('amount').value;
+    const message = document.getElementById('message').value;
 
-console.log('ID parameter:', id);
+    if (!iban) {
+        alert("IBAN chybí v URL (?iban=...)");
+        return;
+    }
+
+    await init(); // loads WASM
+
+    const svg = create_qr(iban, amount, message);
+    document.getElementById("qr").innerHTML = svg;
+}
+
+// Attach event listener instead of inline onclick
+document.getElementById('generate').addEventListener('click', run);
